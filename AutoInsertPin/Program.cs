9856e2d51b4945385392cd8245577676534ee608
Keith MacDonald <km@textpad.com>
2024-12-15 16:18:16 +0000

    Modified to handle the XAML UI of signtool and to use .NET Framework 4.8.1.

--- AutoInsertPin/Program.cs
+++ AutoInsertPin/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Test.Input;
+﻿// Usage AutoInsertPin.exe [pin]
+// If the pin is not supplied on the command line, it will be prompted for.
+// Modified to work with the XAML UI of signtool.exe
+
 using System;
 using System.Threading.Tasks;
 using System.Windows.Automation;
@@ -10,11 +13,18 @@ namespace AutoInsertPin
     {
         static async Task Main(string[] args)
         {
+            string pin;
+
             if (args.Length == 0)
             {
-                Console.Error.WriteLine("Error: Not passed the password as first argument.");
-                return;
+                Console.WriteLine("Enter PIN:");
+                pin = Console.ReadLine();
+
+                if (pin.Length == 0)
+                    return;
             }
+            else
+                pin = args[0];
 
             Console.WriteLine("Waiting for windows. Press Ctrl+C to finish monitoring windows...");
 
@@ -23,34 +33,30 @@ namespace AutoInsertPin
                 try
                 {
                     var window = AutomationElement.RootElement.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.NameProperty, "Windows Security"));
+                    await Task.Delay(1000);
                     if (window == null)
-                    {
-                        await Task.Delay(1000);
                         continue;
 
-                    }
-                    await Task.Delay(1000);
+                    // Console.WriteLine("Located window");
 
-                    Console.WriteLine("Located window");
-
-                    var editControl = window.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.NameProperty, "PIN"));
+                    var editControl = window.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
                     if (editControl == null)
                         continue;
 
                     editControl.SetFocus();
-                    SendKeys.SendWait("^(a)");
+                    SendKeys.SendWait("^(a)");      // select all
                     await Task.Delay(250);
-                    SendKeys.SendWait("{DELETE}");
+                    SendKeys.SendWait("{DELETE}");  // delete
                     await Task.Delay(250);
-                    SendKeys.SendWait(args[0]);
+                    SendKeys.SendWait(pin);         // fill in the PIN
                     await Task.Delay(250);
 
                     var okControl = window.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.NameProperty, "OK"));
                     if (okControl == null)
                         continue;
 
-                    Mouse.MoveTo(new System.Drawing.Point((int)okControl.GetClickablePoint().X, (int)okControl.GetClickablePoint().Y));
-                    Mouse.Click(MouseButton.Left);
+                    var invokePattern = okControl.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+                    invokePattern.Invoke();         // click OK
 
                     await Task.Delay(1000);
                 }
